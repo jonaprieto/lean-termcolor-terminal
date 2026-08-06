da2043a22afb86f022550e5dcd48abfa0372f34b
Jonathan Cubides <prieto.jona@gmail.com>
2026-08-06 01:53:12 -0500

    chore(terminal): refresh widgets release

--- lakefile.lean
+++ lakefile.lean
@@ -2,7 +2,7 @@ import Lake
 open Lake DSL
 
 package «termcolor-terminal» where
-  version := v!"0.1.10"
+  version := v!"0.1.11"
   leanOptions := #[⟨`autoImplicit, false⟩, ⟨`relaxedAutoImplicit, false⟩]
 
 require «termcolor» from git
@@ -15,7 +15,7 @@ require «termcolor-layout» from git
 
 require «termcolor-widgets» from git
   "https://github.com/jonaprieto/lean-termcolor-widgets.git"
-  @ "v0.1.6"
+  @ "v0.1.8"
 
 @[default_target]
 lean_lib «TermColor.Terminal» where
