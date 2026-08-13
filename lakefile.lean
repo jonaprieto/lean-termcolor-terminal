c960e2e8f34b6c404b058eec5db79a53c27b5962
Jonathan Cubides <prieto.jona@gmail.com>
2026-08-12 21:29:43 -0500

    build: adopt Lean v4.33.0

--- lakefile.lean
+++ lakefile.lean
@@ -2,7 +2,7 @@ import Lake
 open Lake DSL
 
 package «termcolor-terminal» where
-  version := v!"0.3.2"
+  version := v!"0.3.3"
   leanOptions := #[⟨`autoImplicit, false⟩, ⟨`relaxedAutoImplicit, false⟩]
 
 require «termcolor» from git
